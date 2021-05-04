9cc16ead260e66d1af5e86466dca1d79a47399f6
Niko Hujanen <niko.hujanen@gmail.com>
2021-05-04 18:38:28 +0300

    New hello page

--- app/ExampleModule.php
+++ app/ExampleModule.php
@@ -1,36 +1,58 @@
 <?php
 
-use Apitin\Database;
 use Apitin\Module;
 use Apitin\Router;
 
 class ExampleModule extends Module
 {
-    /**
-     * Build routes for module
-     */
     public function route(Router $router): void
     {
-        // Route / to: $this->test(...)
         $router->get('*', function($uri) {
-            return $this->call('test', [
-                'uri' => $uri,
-            ]);
+            echo $this->call('help');
+            exit;
         });
     }
 
-    /**
-     * Callback handler for a route
-     * 
-     * Database implements DI class
-     * -> $db will be automatically populated with Database::factory()
-     */
-    public function test($uri, Database $db)
+    public function help()
     {
-        return [
-            'uri' => $uri,
-            'foo' => $db->one('SELECT CURRENT_TIMESTAMP()'),
-            'bar' => config('DATABASE_HOSTNAME', ''),
-        ];
+        $thisModule = get_class($this);
+        $thisMethod = __FUNCTION__;
+        $thisFile   = realpath(__FILE__);
+        $configPath = realpath(APP_PATH . '/.env');
+
+        return <<<EOF
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>Welcome to Apitin project</title>
+        <style>kbd { display: inline-block; background: #333; color: #ccc; padding: 0.15em 0.25em; margin: 0.1em; border-radius: 3px; }</style>
+        <script src="https://unpkg.com/jquery@3.3.1/dist/jquery.js"></script>
+        <link rel="stylesheet" type="text/css" href="https://unpkg.com/fomantic-ui@2.8.7/dist/semantic.min.css">
+        <script src="https://unpkg.com/fomantic-ui@2.8.7/dist/semantic.min.js"></script
+    </head>
+    <body>
+        <main class="ui container">
+            <div class="ui padded basic segment">
+                <h1 class="ui center aligned icon header">
+                    <i class="circular flask icon"></i>
+                    Welcome to Apitin!
+                </h1>
+            </div>
+            <div class="ui divider"></div>
+            <div class="ui padded basic segment">
+                <h2 class="ui header">
+                    General information
+                </h2>
+                <p>
+                    You are currently viewing result of <kbd>{$thisModule}->{$thisMethod}()</kbd> at <kbd>{$thisFile}</kbd>.
+                </p>
+                <p>
+                    You should add your database credentials to <kbd>{$configPath}</kbd>.
+                </p>
+            </div>
+        </main>
+    </body>
+</html>
+EOF;
     }
 }
